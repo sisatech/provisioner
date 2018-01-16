d83bd25b9d84fa9f0031bc94d97277593132b891
Joel Smith <joel.smith@sisa-tech.com>
2018-01-16 15:46:42 +1000

    Finished amazon s3

--- pkg/amazon/provisioner.go
+++ pkg/amazon/provisioner.go
@@ -1,30 +1,34 @@
 package amazon
 
 import (
+	"bytes"
 	"context"
-	"flag"
-	"fmt"
 	"io"
-	"os"
+	"io/ioutil"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 // Provisioner ...
 type Provisioner struct {
-	Description string
-	format      string
-	bucketName  string
+	bucket  *string       // aws s3 bucket name to upload the file to
+	timeout time.Duration // timeot, time to do the upload within
 }
 
 // NewClient ...
-func NewClient() (*Provisioner, error) {
+func NewClient(bucket string, timeout string) (*Provisioner, error) {
 	p := new(Provisioner)
+
+	var err error
+
+	p.bucket = aws.String(bucket)
+	p.timeout, err = time.ParseDuration(timeout)
+	if err != nil {
+		return nil, err
+	}
 
 	return p, nil
 }
@@ -32,42 +36,37 @@ func NewClient() (*Provisioner, error) {
 // Provision ...
 func (p *Provisioner) Provision(f string, r io.ReadCloser) error {
 
-	var bucket, key string
-	var timeout time.Duration
-
-	flag.StringVar(&bucket, "b", "", "Bucket name.")
-	flag.StringVar(&key, "k", "", "Object key name.")
-	flag.DurationVar(&timeout, "d", 0, "Upload timeout.")
-	flag.Parse()
-
 	sess := session.Must(session.NewSession())
 	svc := s3.New(sess)
 
 	ctx := context.Background()
+
+	// Set the timeout
 	var cancelFn func()
-	if timeout > 0 {
-		ctx, cancelFn = context.WithTimeout(ctx, timeout)
+	if p.timeout > 0 {
+		ctx, cancelFn = context.WithTimeout(ctx, p.timeout)
 	}
 
 	defer cancelFn()
 
-	_, err := svc.PutObjectWithContext(ctx, &s3.PutObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-		Body:   os.Stdin,
+	// We need to conver ReadCloser to ReadSeeker
+	// convert to []byte
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	// convert to Reader
+	data := bytes.NewReader(b)
+
+	// Uploads the file f into the s3 bucket with the data contained within r.
+	_, err = svc.PutObjectWithContext(ctx, &s3.PutObjectInput{
+		Bucket: p.bucket,
+		Key:    aws.String(f),
+		Body:   data,
 	})
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == request.CanceledErrorCode {
-			// If the SDK can determine the request or retry delay was canceled
-			// by a context the CanceledErrorCode error code will be returned.
-			fmt.Fprintf(os.Stderr, "upload canceled due to timeout, %v\n", err)
-		} else {
-			fmt.Fprintf(os.Stderr, "failed to upload object, %v\n", err)
-		}
-		os.Exit(1)
+		return err
 	}
-
-	fmt.Printf("successfully uploaded file to %s/%s\n", bucket, key)
 
 	return nil
 }
--- pkg/provisioner/provisioner.go
+++ pkg/provisioner/provisioner.go
@@ -3,6 +3,7 @@ package provisioner
 import (
 	"io"
 
+	"github.com/sisatech/provisioner/pkg/amazon"
 	"github.com/sisatech/provisioner/pkg/google"
 	"github.com/sisatech/provisioner/pkg/microsoft"
 	"github.com/sisatech/provisioner/pkg/vmware"
@@ -13,17 +14,22 @@ type Provisioner interface {
 	Provision(string, io.ReadCloser) error
 }
 
+// NewAmazon ...
+func NewAmazon(cfg *amazon.Config) (Provisioner, error) {
+	return amazon.NewClient(cfg)
+}
+
 // NewGCP ...
 func NewGCP(bucket string, key *google.Key) (Provisioner, error) {
 	return google.NewClient(bucket, key)
 }
 
-// NewVMWare ...
-func NewVMWare(cfg *vmware.Config) (Provisioner, error) {
-	return vmware.NewClient(cfg)
-}
-
 // NewMicrosoft ...
 func NewMicrosoft(cfg *microsoft.Config) (Provisioner, error) {
 	return microsoft.NewClient(cfg)
+}
+
+// NewVMWare ...
+func NewVMWare(cfg *vmware.Config) (Provisioner, error) {
+	return vmware.NewClient(cfg)
 }
